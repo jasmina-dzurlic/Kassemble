e9ca072da9ca851412c55d99e528e3b6d461c48d
Jasmina Dzurlic <75990883+jasmina-dzurlic@users.noreply.github.com>
2021-03-08 02:56:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python
 
+"""
+Call `pip install -e .` to install package locally for testing.
+"""
+
+from setuptools import setup
+
+# Build setup.
+setup(
+    name="Kassemble",
+    version="0.0.1",
+    url="https://github.com/jasmina-dzurlic/Kassemble.git",
+    author="Jasmina Dzurlic",
+    author_email="jd3451@columbia.edu",
+    description="A package to asseble denovo contigs with a reference-free, k-mer based approach.",
+)
