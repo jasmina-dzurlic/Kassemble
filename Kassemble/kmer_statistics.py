43149e97ba23a614b8ee1682f719986c4cb8dd76
Jasmina Dzurlic <75990883+jasmina-dzurlic@users.noreply.github.com>
2021-04-18 03:31:04 -0400

    Update kmer_statistics.py

--- Kassemble/kmer_statistics.py
+++ Kassemble/kmer_statistics.py
@@ -1,1 +1,24 @@
 
+#!/usr/bin/env python
+
+"""
+Producing assembly statisitcs with The K-mer Analysis Toolkit (KAT)
+(https://github.com/TGAC/KAT)
+
+Install:
+bioconda install kat
+
+optional arguments:
+--hist 
+--gcp
+--comp
+--sect
+--filter
+--denisty
+--spectra-hist
+
+"""
+
+import subprocess
+
+
